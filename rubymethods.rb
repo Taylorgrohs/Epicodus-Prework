f2065b88165c36e638c77863acb2505e5d7d9f33
Taylor Grohs <Taylorgrohs@gmail.com>
2015-08-20 16:22:07 -0600

    added ruby methods

---
+++ rubymethods.rb
@@ -0,0 +1,145 @@
+class Array
+define_method(:remove) do |gone|
+
+
+unpop
+
+list = [1, 2, 3, 4, 5, 6]
+4.times() do
+list.unpop()
+end
+
+num = 5
+5.times() do
+num.*(num)
+end
+
+friends = ["Liz", "Sally", "Jo"]
+friends.each() do |friend|
+friend.concat(" loves programming!")
+end
+friend = ["kon", "nate", "shane"]
+class Array
+define_method(:upcase) do 
+friend.each() do|string|
+upcased_string = string.upcase()
+friend.push!(upcased_string)
+end
+end
+
+names = ["michael", "maureen", "dustin", "kelly"]
+names.each() do |upcased|
+upcased.upcase!()
+end
+
+numbers = [1, 2, 3]
+numbers.each() do |number|
+numbers = number.+(1)
+numbers.push(number)
+end
+
+numbers = (1..10)
+sum = 1
+numbers.each() do |number|
+sum = sum.*(number)
+end
+
+my_fave_drink = "monster"
+my_new_drink = []
+my_fave_drink = my_fave_drink.split(//)
+my_fave_drink.each() do |letter|
+my_new_drink.push(letter)
+my_new_drink.push(letter)
+my_new_drink.push(letter)
+my_fave_drink = my_new_drink.join("")
+end
+
+my_fave_drink = "monster"
+my_new_drink = []
+my_fave_drink = my_fave_drink.split(//)
+my_fave_drink.each() do |letter|
+3.times() do
+my_new_drink.push(letter)
+my_fave_drink = my_new_drink.join("")
+end
+end
+
+ num = []
+
+ 5.times() do |number|
+ num.push(number)
+ num.push(number)
+ num.push(number)
+ end
+
+class Array
+define_method(:upcase) do
+new_array = []
+self.each() do |string|
+upcased_string = string.upcase()
+new_array.push(upcased_string)
+end
+new_array
+end
+end
+
+class Range
+define_method(:sum) do
+total = 0
+self.each() do |number|
+total = total.+(number)
+end
+total
+end
+end
+
+drinks = ["pepsi", "coke", "pepper"]
+class Array
+define_method(:upcase!) do
+self.each() do |string|
+upcased_string = string.upcase!()
+end
+end
+end
+
+class Range
+define_method(:product) do
+product = 1
+self.each() do |number|
+product = product.*(number)
+end
+product
+end
+end
+
+
+
+class String
+define_method(:triple) do
+hold = []
+add = []
+hold = self.split(//)
+hold.each() do |x|
+3.times() do
+add.push(x)
+end
+end
+add = add.join("")
+add
+end
+end
+
+class String
+define_method(:double) do
+hold = []
+add = []
+hold = self.split(//)
+hold.each() do |x|
+2.times() do
+add.push(x)
+end
+end
+add = add.join("")
+add
+end
+end
